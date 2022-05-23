538de21b555f0bdc7a83a0a4fd2361b39b0936c4
Colby Patterson <cpatt@byui.edu>
2022-05-23 11:38:57 -0600

    Working on demo app

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+const Card = () => {
+  return (
+    <div className="container">
+      <div className="bio">
+      <h3>User Name</h3>
+      </div>
+      <div className="about">
+        <p>About user</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/JSXDemo.js
@@ -0,0 +1,73 @@
+// JSX is not a string and it is not HTML
+// It is an extenstion to JavaScript
+// It allows us to write 'HTML' in JS
+
+// This is a React functional component (preferred)
+// This is a function => returns JSX
+function JSXDemo() {
+  let name = "Joey";
+  let num = 2
+  let showThing = true
+  let hideThing = true
+  // function return a string
+  const sayHello = () => {
+    return `Hello`;
+  };
+
+  const getJSX = () => {
+    return <p>this is jsx</p>;
+  };
+  const getMultilineJSX = () => {
+    return (
+      <div>
+        <h1>YO</h1>
+        <p>this is jsx</p>
+      </div>
+    );
+  };
+
+  const renderArrayOfObject = ()=>{
+    let people = [{name:'tony', age:22},{name:'Jill',age:45}]
+    return people.map((p) => {
+      return (
+        <div>
+          <p>Name: {p.name}</p>
+          <p>Age: {p.age}</p>
+        </div>
+      )
+    })
+  }
+  console.log('YO')
+  return (
+    <div className="App">
+      <h1>Hello JSX World</h1>
+      <p>1 + 1 = {1 + 1}</p>
+      <h3>call a function that returns a string</h3>
+      <p>
+        {sayHello()} {name}
+      </p>
+      <h3>call a function that returns a jsx</h3>
+      {getJSX()}
+      <h3>call a function that returns more jsx</h3>
+      {getMultilineJSX()}
+      <h3>call a built js function</h3>
+      <p>{new Date().toDateString()}</p>
+      <h3>render array of things</h3>
+      <p>{[1,2,3,4]}</p>
+      <h3>render array of JSX</h3>
+      <ul>{[<li>1</li>,<li>2</li>,<li>3</li>]}</ul>
+      <h3>render Objects Breaks</h3>
+      {/* <p>{name:'Tony'}</p> */}
+      <h3>render Objects With JSON.stringify</h3>
+      <p>{JSON.stringify({name:'Tony'})}</p>
+
+      <h3>render array of Objects</h3>
+      {renderArrayOfObject()}
+      <hr />
+      <h3>Ternary/shorthand with booleans</h3>
+      <p>{num} is {num % 2 === 0 ? 'even':'odd'}</p>
+    </div>
+  );
+}
+
+export default JSXDemo;
